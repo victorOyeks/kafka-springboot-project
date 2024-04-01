0d8f80580e05db23c56022b93d2e80d1dabc4fc9
Victor Oyeks <Oyeks@Victors-MacBook-Pro.local>
2024-04-01 23:56:04 +0100

    first commit

---
+++ src/main/java/com/oyeks/kafka/KafkaRealWorldApplication.java
@@ -0,0 +1,13 @@
+package com.oyeks.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRealWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRealWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oyeks/kafka/KafkaRealWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oyeks.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRealWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
